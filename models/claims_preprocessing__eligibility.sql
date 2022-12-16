09eb242fe15593f88a611617afb6b91b0c5e9390
Matt Vivier <matt.vivier@empassion.com>
2022-12-16 17:19:29 -0500

    fix casting of elig fields

--- models/claims_preprocessing__eligibility.sql
+++ models/claims_preprocessing__eligibility.sql
@@ -9,17 +9,17 @@
 {{ config(enabled=var('claims_preprocessing_enabled',var('tuva_packages_enabled',True))) }}
 
 select distinct
-    cast(elig.patient_id as varchar) as patient_id
-    , cast(elig.member_id as varchar) as member_id
+    {{ cast_string_or_varchar('elig.patient_id') }} as patient_id
+    , {{ cast_string_or_varchar('elig.member_id') }} as member_id
     , date(elig.enrollment_start_date) as enrollment_start_date
     , date(elig.enrollment_end_date) as enrollment_end_date
-    , cast(elig.payer as varchar) as payer
-    , cast(elig.payer_type as varchar) as payer_type
-    , cast(elig.dual_status_code as varchar) as dual_status_code
-    , cast(dual.dual_status_description as varchar) as dual_status_description
-    , cast(elig.medicare_status_code as varchar) as medicare_status_code
-    , cast(status.medicare_status_description as varchar) as medicare_status_description
-    , cast(elig.data_source as varchar) as data_source
+    , {{ cast_string_or_varchar('elig.payer') }} as payer
+    , {{ cast_string_or_varchar('elig.payer_type') }} as payer_type
+    , {{ cast_string_or_varchar('elig.dual_status_code') }} as dual_status_code
+    , {{ cast_string_or_varchar('dual.dual_status_description') }} as dual_status_description
+    , {{ cast_string_or_varchar('elig.medicare_status_code') }} as medicare_status_code
+    , {{ cast_string_or_varchar('status.medicare_status_description') }} as medicare_status_description
+    , {{ cast_string_or_varchar('elig.data_source') }} as data_source
 from {{ var('eligibility')}} elig
 left join {{ ref('terminology__medicare_dual_eligibility')}} dual
     on elig.dual_status_code = dual.dual_status_code
