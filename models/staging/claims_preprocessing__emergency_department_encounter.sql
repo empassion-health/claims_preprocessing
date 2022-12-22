2369f62910bf1a1fb3247c90848ddba003d1c47e
Matt Vivier <matt.vivier@empassion.com>
2022-12-22 17:22:11 -0500

    Revert "try to work around query complexity"
    This reverts commit faacd1d0ddfe6d516d773001bb3c8fce267ec530.
    
    Again

--- models/staging/claims_preprocessing__emergency_department_encounter.sql
+++ models/staging/claims_preprocessing__emergency_department_encounter.sql
@@ -13,8 +13,8 @@
 ---------------------------------------------------------------------------------------------------------
 {{ config(enabled=var('claims_preprocessing_enabled',var('tuva_packages_enabled',True))) }}
 
-DROP TABLE IF EXISTS claim_emergency_department_eligibility;
-CREATE TABLE claim_emergency_department_eligibility as(
+
+with claim_emergency_department_eligibility as(
   select 
       claim_type
     , med.revenue_center_code
@@ -23,9 +23,8 @@ CREATE TABLE claim_emergency_department_eligibility as(
     , claim_id
   from {{ var('medical_claim')}} med
   where med.revenue_center_code in ('0450','0451','0452','0459','0981')
-);
-DROP TABLE IF EXISTS room_and_board_claims;
-CREATE TABLE room_and_board_claims as(
+)
+, room_and_board_claims as(
   select 
      med.revenue_center_code
     , rev.revenue_center_description
