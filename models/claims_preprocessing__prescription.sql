df59cd3a253297c8af3608d3ac7beda6f838a024
tuvaforrest <forrest@tuvahealth.com>
2023-01-03 11:51:23 -0500

    added logic to use empty pharmacy claim table if variable is set, incremented version number

--- models/claims_preprocessing__prescription.sql
+++ models/claims_preprocessing__prescription.sql
@@ -1,5 +1,12 @@
 {{ config(enabled=var('claims_preprocessing_enabled',var('tuva_packages_enabled',True))) }}
 
+{% if builtins.var('pharmacy_claim')|lower == "none" %}
+{% set source_exists = false %}
+{% else %}
+{% set source_exists = true %}
+{% endif -%}
+
+{% if source_exists %}
 select
     cast(claim_id as {{ dbt.type_string() }}) as claim_id
     , cast(claim_line_number as {{ dbt.type_string() }}) as claim_line_number
@@ -17,3 +24,29 @@ select
     , cast(allowed_amount as numeric ) as allowed_amount
     , cast(data_source as {{ dbt.type_string() }}) as data_source
 from {{ var('pharmacy_claim')}} m
+
+{% else %}
+
+{% if execute %}
+{{- log("pharmacy_claim soruce does not exist, using empty table.", info=true) -}}
+{% endif %}
+select
+    cast(null as {{ dbt.type_string() }}) as claim_id
+    , cast(null as {{ dbt.type_string() }}) as claim_line_number
+    , cast(null as {{ dbt.type_string() }}) as patient_id
+    , cast(null as {{ dbt.type_string() }}) as member_id
+    , cast(null as {{ dbt.type_string() }}) as prescribing_provider_npi
+    , cast(null as {{ dbt.type_string() }}) as dispensing_provider_npi
+    , cast(null as date ) as dispensing_date
+    , cast(null as {{ dbt.type_string() }}) as ndc_code
+    , cast(null as int ) as quantity
+    , cast(null as int ) as days_supply
+    , cast(null as int) as refills
+    , cast(null as date ) as paid_date
+    , cast(null as numeric ) as paid_amount
+    , cast(null as numeric ) as allowed_amount
+    , cast(null as {{ dbt.type_string() }}) as data_source
+    limit 0
+
+{%- endif %}
+
